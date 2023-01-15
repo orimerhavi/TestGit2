6b1ebac4ecd770d33ac482ed4d6a8a184f6c77ad
trpyy <trpttnbrh@gmail.com>
2023-01-15 16:44:00 +0200

    הוספתי print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Ilay ur so GAY!!!");
         System.out.println(error);
+        System.out.println("hjhhjfg76");
+
 
 
     }
