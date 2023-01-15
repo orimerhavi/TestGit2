224a53cda535243ead98ec3aade480a1e77510a1
ori merhavi <orimer14havi@gmail.com>
2023-01-15 15:28:06 +0200

    copy rights

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+/*
+@copyrights: Ori Merhavi 213196769 15.1.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
