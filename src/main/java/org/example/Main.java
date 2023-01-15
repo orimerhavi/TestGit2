0f32a7857033c7e71bf88c30329bbe8befda9acc
orimerhavi <orimer14havi@gmail.com>
2023-01-15 16:19:19 +0200

    copy rights

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Ori ur so  not GAY!!!");
+        System.out.println("Ilay ur so GAY!!!");
 
     }
 }
