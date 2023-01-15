089d1de54a380b48fb2cc01b631fedc9b1f9e9fb
NAVE YADAI <87646702+NAVEYADAI@users.noreply.github.com>
2023-01-15 17:37:49 +0300

    nave push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
+
+import static org.example.learnClass.demo;
+
 /*
 @copyrights: Ori Merhavi 213196769 15.1.2023
  */
 public class Main {
+    final String error=demo;
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Ilay ur so GAY!!!");
+
 
     }
 }
---
+++ src/main/java/org/example/learnClass.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class learnClass {
+    public static String demo = "Ori you are need to learn a java leng' code";
+
+
+}
