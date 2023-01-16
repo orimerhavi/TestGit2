1320af9dd0a72dce6267884591bd8ff21686ca60
orimerhavi <orimer14havi@gmail.com>
2023-01-16 22:12:20 +0200

    I added insert

--- src/main/java/org/example/java.java
+++ src/main/java/org/example/java.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class java {
-    void openConnection()
+    public void openConnection()
     {
         String jdbcURL = "jdbc:postgresql://localhost:5432/LearnGit";
         String userName = "postgres";
@@ -14,6 +12,22 @@ public class java {
         try {
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.print("There is connection");
+
+            String sql ="INSERT INTO learn (first_name, last_name, email) VALUES (?,?,?)";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1,"Avraam");
+            statement.setString(2,"Gidon");
+            statement.setString(3,"avraam.gidon@gmail.com");
+
+
+            int rows = statement.executeUpdate();
+            if(rows > 0){
+                System.out.println("A new contact has been inserted.");
+            }
+
+            connection.close();
         } catch (SQLException e) {
             System.out.print("No Connection");
             throw new RuntimeException(e);
